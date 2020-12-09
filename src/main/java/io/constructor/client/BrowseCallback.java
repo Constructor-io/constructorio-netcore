c1af06e9b583eec7e9e9291f1d4fddf0603f907a
Zubin Tiku <rubikzube@gmail.com>
2020-12-08 22:10:33 -0500

    Restructure

--- src/main/java/io/constructor/client/BrowseCallback.java
+++
@@ -1,7 +0,0 @@
-package io.constructor.client;
-import io.constructor.client.models.BrowseResponse;
-
-public interface BrowseCallback {
-  public void onFailure(ConstructorException exception);
-  public void onResponse(BrowseResponse response) throws ConstructorException;
-}
--- src/main/java/io/constructor/client/SearchCallback.java
+++
@@ -1,7 +0,0 @@
-package io.constructor.client;
-import io.constructor.client.models.SearchResponse;
-
-public interface SearchCallback {
-  public void onFailure(ConstructorException exception);
-  public void onResponse(SearchResponse response) throws ConstructorException;
-}
