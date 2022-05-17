7442502c219e5d1b90383730f0385e5dbadeb51e
stanlp1 <57369888+stanlp1@users.noreply.github.com>
2022-05-17 15:16:15 -0700

    [CSL-1010] Create recommendation endpoints. (#3)
    * Add recommendations
    
    * Fix user info setters
    
    * Remove system import, fix null empty check
    
    * Fix version constant
    
    * Incorporate user info in request
    
    Co-authored-by: Stanley Peng <speng1011@gmaill.com>

--- Constructorio_NET/src/utils/Constants.cs
+++ Constructorio_NET/src/utils/Constants.cs
@@ -3,23 +3,24 @@ namespace Constructorio_NET
 {
     public class Constants
     {
-        internal const string API_KEY = "apiKey";
-        internal const string CLIENT_ID = "clientId";
-        internal const string FILTERS = "filters";
-        internal const string FMT_OPTIONS = "fmtOptions";
-        internal const string HIDDEN_FIELDS = "hiddenFields";
-        internal const string SEGMENTS = "segments";
-        internal const string SESSION_ID = "sessionId";
-        internal const string SECTION = "section";
-        internal const string TEST_CELLS = "testCells";
-        internal const string USER_ID = "userId";
-        internal const string VERSION = "version";
-        internal const string PAGE = "page";
-        internal const string RESULTS_PER_PAGE = "resultsPerPage";
-        internal const string SECURITY_TOKEN = "x-cnstrc-token";
-        internal const string SORT_BY = "sortBy";
-        internal const string SORT_ORDER = "sortOrder";
-        internal const string USER_AGENT = "User-Agent";
-        internal const string USER_IP = "X-Forwarded-For";
+        public const string API_KEY = "key";
+        public const string SERVICE_URL = "service_url";
+        public const string CLIENT_ID = "i";
+        public const string FILTERS = "filters";
+        public const string FMT_OPTIONS = "fmt_options";
+        public const string HIDDEN_FIELDS = "hidden_fields";
+        public const string SEGMENTS = "us";
+        public const string SESSION_ID = "s";
+        public const string SECTION = "section";
+        public const string TEST_CELLS = "test_cells";
+        public const string USER_ID = "ui";
+        public const string VERSION = "c";
+        public const string PAGE = "page";
+        public const string RESULTS_PER_PAGE = "num_results_per_page";
+        public const string SECURITY_TOKEN = "x-cnstrc-token";
+        public const string SORT_BY = "sort_by";
+        public const string SORT_ORDER = "sort_order";
+        public const string USER_AGENT = "User-Agent";
+        public const string USER_IP = "X-Forwarded-For";
     }
 }
