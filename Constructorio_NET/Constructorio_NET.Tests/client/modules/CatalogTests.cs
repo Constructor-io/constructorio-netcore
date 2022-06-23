50b17779be8bfbde138342d130028b8bb7d521f4
Christopher Gee <christopherrgee@gmail.com>
2022-06-23 13:47:37 -0700

    Add token json file

--- Constructorio_NET/Constructorio_NET.Tests/client/modules/CatalogTests.cs
+++ Constructorio_NET/Constructorio_NET.Tests/client/modules/CatalogTests.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using System;
 using System.Collections;
@@ -17,15 +18,17 @@ namespace Constructorio_NET.Tests
         private string Query = "item";
         private int SessionId = 4;
         private Hashtable UserParameters = new Hashtable();
-        private string url = "https://raw.githubusercontent.com/Constructor-io/integration-examples/main/catalog/";
 
         [SetUp]
         public void Setup()
         {
+            var myJObject = JObject.Parse(File.ReadAllText("./../../../../../.config/local.json"));
+            string testApiToken = myJObject.SelectToken("TEST_API_TOKEN").Value<string>();
+
             this.Options = new Hashtable()
             {
                 { Constants.API_KEY, this.ApiKey },
-                { "apiToken", "95bk4jJxsYMFYbXs9B4V6CW8gUEQCh" },
+                { "apiToken", testApiToken },
             };
             this.UserParameters = new Hashtable()
             {
@@ -35,18 +38,45 @@ namespace Constructorio_NET.Tests
         }
 
         [Test]
-        public void UpdateCatalog()
+        public void ReplaceCatalog()
         {
-            StreamContent stream = new StreamContent(File.OpenRead("./../../../resources/items.csv"));
+            StreamContent stream = new StreamContent(File.OpenRead("./../../../resources/csv/items.csv"));
             stream.Headers.ContentType = new MediaTypeHeaderValue("text/csv");
             Dictionary<string, StreamContent> files = new Dictionary<string, StreamContent>()
             {
                 { "items", stream }
             };
-            
+            ConstructorIO constructorio = new ConstructorIO(this.Options);
+            CatalogRequest req = new CatalogRequest(files);
+            constructorio.Catalog.ReplaceCatalog(req);
+        }
+
+        [Test]
+        public void UpdateCatalog()
+        {
+            StreamContent stream = new StreamContent(File.OpenRead("./../../../resources/csv/items.csv"));
+            stream.Headers.ContentType = new MediaTypeHeaderValue("text/csv");
+            Dictionary<string, StreamContent> files = new Dictionary<string, StreamContent>()
+            {
+                { "items", stream }
+            };
             ConstructorIO constructorio = new ConstructorIO(this.Options);
             CatalogRequest req = new CatalogRequest(files);
             constructorio.Catalog.UpdateCatalog(req);
+        }
+
+        [Test]
+        public void PatchCatalog()
+        {
+            StreamContent stream = new StreamContent(File.OpenRead("./../../../resources/csv/items.csv"));
+            stream.Headers.ContentType = new MediaTypeHeaderValue("text/csv");
+            Dictionary<string, StreamContent> files = new Dictionary<string, StreamContent>()
+            {
+                { "items", stream }
+            };
+            ConstructorIO constructorio = new ConstructorIO(this.Options);
+            CatalogRequest req = new CatalogRequest(files);
+            constructorio.Catalog.PatchCatalog(req);
         }
     }
 }
