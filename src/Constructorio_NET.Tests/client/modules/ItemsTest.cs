3ac9e8908ecb667c7f35725b0e0f0e81f54ae9c9
stanlp1 <57369888+stanlp1@users.noreply.github.com>
2022-08-17 16:07:52 -0700

    Tests

---
+++ src/Constructorio_NET.Tests/client/modules/ItemsTest.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Constructorio_NET.Models;
+using Constructorio_NET.Models.Items;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Constructorio_NET.Tests
+{
+    [TestFixture]
+    public class ItemsTest
+    {
+        private readonly string ApiKey = "ZqXaOfXuBWD4s3XzCI1q";
+        private ConstructorioConfig Config;
+
+        [OneTimeSetUp]
+        public async Task Setup()
+        {
+            JObject json = JObject.Parse(File.ReadAllText("./../../../../../.config/local.json"));
+            string testApiToken = json.SelectToken("TEST_API_TOKEN").Value<string>();
+
+            this.Config = new ConstructorioConfig(this.ApiKey)
+            {
+                ApiToken = testApiToken
+            };
+
+            var constructorio = new ConstructorIO(this.Config);
+            List<ConstructorItem> items = new List<ConstructorItem>();
+            ConstructorItem item = TestUtils.CreateRandomItem();
+            items.Add(item);
+            await constructorio.Items.CreateOrReplaceItems(items, "Products", false, "stanley.peng@constructor.io");
+
+            List<ConstructorVariation> variations = new List<ConstructorVariation>();
+            ConstructorVariation variation = TestUtils.CreateRandomVariation("random-id");
+            variations.Add(variation);
+            await constructorio.Items.CreateOrReplaceVariations(variations, "Products");
+        }
+
+        [OneTimeTearDown]
+        public async Task Cleanup()
+        {
+            await TestUtils.Cleanup();
+        }
+
+        [Test]
+        public async Task CreateItemsWithValidOptionsShouldReturnTrue()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            List<ConstructorItem> items = new List<ConstructorItem>();
+            ConstructorItem item = TestUtils.CreateRandomItem();
+            items.Add(item);
+            bool result = await constructorio.Items.CreateOrReplaceItems(items, "Products");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task CreateItemsWithOptionalOptionsShouldReturnTrue()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            List<ConstructorItem> items = new List<ConstructorItem>();
+            ConstructorItem item = TestUtils.CreateRandomItem();
+            items.Add(item);
+            bool result = await constructorio.Items.CreateOrReplaceItems(items, "Products", false, "stanley.peng@constructor.io");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task UpdateItemsShouldReturnTrue()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            List<ConstructorItem> items = new List<ConstructorItem>();
+            ConstructorItem item = TestUtils.PeekItem();
+            items.Add(item);
+            bool result = await constructorio.Items.UpdateItems(items, "Products");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task UpdateItemsWithOptionalOptionsShouldReturnTrue()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            List<ConstructorItem> items = new List<ConstructorItem>();
+            ConstructorItem item = TestUtils.PeekItem();
+            items.Add(item);
+            bool result = await constructorio.Items.UpdateItems(items, "Products", false, "stanley.peng@constructor.io");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task RetrieveItemsShouldReturnValidItems()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            ItemsRequest request = new ItemsRequest();
+            ItemsResponse result = await constructorio.Items.RetrieveItems(request);
+            Assert.IsTrue(int.Parse(result.TotalCount) > 0);
+            Assert.IsTrue(result.Items.Count > 0);
+        }
+
+        [Test]
+        public async Task RetrieveItemsWithAnIdShouldReturnValidItems()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            ItemsRequest request = new ItemsRequest();
+            request.Ids = new List<string>() { "10001" };
+            ItemsResponse result = await constructorio.Items.RetrieveItems(request);
+            Assert.IsTrue(int.Parse(result.TotalCount) == 1);
+            Assert.IsTrue(result.Items.Count == 1);
+        }
+
+        [Test]
+        public async Task RetrieveItemsWithMultipleIdsShouldReturnValidItems()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            ItemsRequest request = new ItemsRequest();
+            request.Ids = new List<string>() { "10001", "10002" };
+            ItemsResponse result = await constructorio.Items.RetrieveItems(request);
+            Assert.IsTrue(int.Parse(result.TotalCount) == 2);
+            Assert.IsTrue(result.Items.Count == 2);
+        }
+
+        [Test]
+        public async Task DeleteItemsWithValidOptionsShouldReturnTrue()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            List<ConstructorItem> items = new List<ConstructorItem>();
+            ConstructorItem item = new ConstructorItem();
+            item.Id = "testItem";
+            item.Name = "testName";
+            item.Url = "testurl";
+            item.ImageUrl = "testimageurl";
+            items.Add(item);
+            bool result = await constructorio.Items.DeleteItems(items, "Products");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task CreateVariationsWithValidOptionsShouldReturnTrue()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            List<ConstructorVariation> variations = new List<ConstructorVariation>();
+            ConstructorVariation variation = TestUtils.CreateRandomVariation("random-id");
+            variations.Add(variation);
+            bool result = await constructorio.Items.CreateOrReplaceVariations(variations, "Products");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task CreateVariationsWithOptionalOptionsShouldReturnTrue()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            List<ConstructorVariation> variations = new List<ConstructorVariation>();
+            ConstructorVariation variation = TestUtils.CreateRandomVariation("random-id");
+            variations.Add(variation);
+            bool result = await constructorio.Items.CreateOrReplaceVariations(variations, "Products", false, "stanley.peng@constructor.io");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task UpdateVariationsShouldReturnTrue()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            List<ConstructorVariation> items = new List<ConstructorVariation>();
+            ConstructorVariation item = TestUtils.PeekVariation();
+            items.Add(item);
+            bool result = await constructorio.Items.UpdateVariations(items, "Products");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task UpdateVariationsWithOptionalOptionsShouldReturnTrue()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            List<ConstructorVariation> items = new List<ConstructorVariation>();
+            ConstructorVariation item = TestUtils.PeekVariation();
+            items.Add(item);
+            bool result = await constructorio.Items.UpdateVariations(items, "Products", false, "stanley.peng@constructor.io");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task RetrieveVariationsShouldReturnValidItems()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            VariationsRequest request = new VariationsRequest();
+            VariationsResponse result = await constructorio.Items.RetrieveVariations(request);
+            Assert.IsTrue(int.Parse(result.TotalCount) > 0);
+            Assert.IsTrue(result.Variations.Count > 0);
+        }
+
+        [Test]
+        public async Task RetrieveVariationsWithAnIdShouldReturnValidItems()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            ItemsRequest request = new ItemsRequest();
+            request.Ids = new List<string>() { "10001" };
+            ItemsResponse result = await constructorio.Items.RetrieveItems(request);
+            Assert.IsTrue(int.Parse(result.TotalCount) == 1);
+            Assert.IsTrue(result.Items.Count == 1);
+        }
+
+        [Test]
+        public async Task RetrieveVariationsWithMultipleIdsShouldReturnValidItems()
+        {
+            var constructorio = new ConstructorIO(this.Config);
+            ItemsRequest request = new ItemsRequest();
+            request.Ids = new List<string>() { "10001", "10002" };
+            ItemsResponse result = await constructorio.Items.RetrieveItems(request);
+            Assert.IsTrue(int.Parse(result.TotalCount) == 2);
+            Assert.IsTrue(result.Items.Count == 2);
+        }
+    }
+}
