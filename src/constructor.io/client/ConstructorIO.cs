83ad0042ef8f8312cd29d4a339600d445daf14cf
Christopher Gee <christopherrgee@gmail.com>
2022-11-08 11:18:57 -0800

    Version bump

--- src/constructor.io/client/ConstructorIO.cs
+++ src/constructor.io/client/ConstructorIO.cs
@@ -11,7 +11,7 @@ namespace Constructorio_NET
     /// </summary>
     public class ConstructorIO
     {
-        private readonly string Version = "cionet-2.1.0";
+        private readonly string Version = "cionet-2.1.1";
         public Autocomplete Autocomplete { get; }
         public Browse Browse { get; }
         public Catalog Catalog { get; }
