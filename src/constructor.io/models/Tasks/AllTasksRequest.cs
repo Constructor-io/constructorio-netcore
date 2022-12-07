32b4979363e2210adf09ffb56740689783a04e82
Christopher Gee <christopherrgee@gmail.com>
2022-12-07 11:52:49 -0800

    Lint

--- src/constructor.io/models/Tasks/AllTasksRequest.cs
+++ src/constructor.io/models/Tasks/AllTasksRequest.cs
@@ -16,7 +16,7 @@ namespace Constructorio_NET.Models
         public string StartDate { get; set; }
         public string EndDate { get; set; }
         public string Status { get; set; }
-        public string Type { get ; set; }
+        public string Type { get; set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AllTasksRequest"/> class.
