bf4a67bc720561dd5018120652e421d6326251cf
Chris Gee <45833304+crgee1@users.noreply.github.com>
2023-01-23 15:47:18 -0800

    [CSL 1983] Add support for item groups (#25)
    * Create item groups model
    
    * Add GetItemGroup, AddItemGroups, and DeleteItemGroups
    
    * Update error message
    
    * Add item group functions
    
    * Add tests
    
    * Update tests
    
    * Remove writeline
    
    * Lint
    
    * Lint
    
    * Typo fix

--- src/Constructorio_NET.Tests/client/modules/BrowseTests.cs
+++ src/Constructorio_NET.Tests/client/modules/BrowseTests.cs
@@ -40,7 +40,7 @@ namespace Constructorio_NET.Tests
             BrowseRequest req = new BrowseRequest(this.FilterName, this.FilterValue);
             ConstructorIO constructorio = new ConstructorIO(new ConstructorioConfig("invalidKey"));
             var ex = Assert.ThrowsAsync<ConstructorException>(() => constructorio.Browse.GetBrowseResults(req));
-            Assert.IsTrue(ex.Message == "Http[400]: We have no record of this key. You can find your key at app.constructor.io/dashboard.", "Correct Error is Returned");
+            Assert.IsTrue(ex.Message == "Http[400]: You have supplied an invalid `key` or `autocomplete_key`. You can find your key at app.constructor.io/dashboard/accounts/api_integration.", "Correct Error is Returned");
         }
 
         [Test]
