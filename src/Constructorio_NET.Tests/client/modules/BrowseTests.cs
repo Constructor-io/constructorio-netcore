059144e441cec4f953cdd4493d1f081c552358ac
Stanley Peng <speng1011@gmail.com>
2023-01-30 16:06:08 -0800

    Change keys used

--- src/Constructorio_NET.Tests/client/modules/BrowseTests.cs
+++ src/Constructorio_NET.Tests/client/modules/BrowseTests.cs
@@ -11,7 +11,7 @@ namespace Constructorio_NET.Tests
     [TestFixture]
     public class BrowseTest
     {
-        private readonly string ApiKey = "ZqXaOfXuBWD4s3XzCI1q";
+        private readonly string ApiKey = "key_vM4GkLckwiuxwyRA";
         private readonly string ClientId = "r4nd-cl1ent-1d";
         private readonly int SessionId = 4;
         private readonly string FilterName = "Color";
