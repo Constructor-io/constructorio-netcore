6cc08c83c093fa5c5c4e56b2f422781d3f596a21
Christopher Gee <christopherrgee@gmail.com>
2023-07-21 09:29:58 -0700

    interface skeleton

---
+++ src/constructor.io/models/common/IFilterable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace Constructorio_NET.Models
+{
+  interface IFilterable
+  {
+  }
+}
