b7fccf2fbf6b805d16d92191eecd086d74c71e5a
Christopher Gee <christopherrgee@gmail.com>
2023-07-21 11:19:50 -0700

    Version Bump

--- docs/search/classes_c.js
+++ docs/search/classes_c.js
@@ -5,13 +5,14 @@ var searchData=
   ['recommendationsrequest_2',['RecommendationsRequest',['../class_constructorio___n_e_t_1_1_models_1_1_recommendations_request.html',1,'Constructorio_NET::Models']]],
   ['recommendationsresponse_3',['RecommendationsResponse',['../class_constructorio___n_e_t_1_1_models_1_1_recommendations_response.html',1,'Constructorio_NET::Models']]],
   ['recommendationsresponseinner_4',['RecommendationsResponseInner',['../class_constructorio___n_e_t_1_1_models_1_1_recommendations_response_inner.html',1,'Constructorio_NET::Models']]],
-  ['redirect_5',['Redirect',['../class_constructorio___n_e_t_1_1_models_1_1_redirect.html',1,'Constructorio_NET::Models']]],
-  ['redirectdata_6',['RedirectData',['../class_constructorio___n_e_t_1_1_models_1_1_redirect_data.html',1,'Constructorio_NET::Models']]],
-  ['refinedcontent_7',['RefinedContent',['../class_constructorio___n_e_t_1_1_models_1_1_refined_content.html',1,'Constructorio_NET::Models']]],
-  ['result_8',['Result',['../class_constructorio___n_e_t_1_1_models_1_1_result.html',1,'Constructorio_NET::Models']]],
-  ['resultdata_9',['ResultData',['../class_constructorio___n_e_t_1_1_models_1_1_result_data.html',1,'Constructorio_NET::Models']]],
-  ['resultfacet_10',['ResultFacet',['../class_constructorio___n_e_t_1_1_models_1_1_result_facet.html',1,'Constructorio_NET::Models']]],
-  ['resultgroup_11',['ResultGroup',['../class_constructorio___n_e_t_1_1_models_1_1_result_group.html',1,'Constructorio_NET::Models']]],
-  ['resultpod_12',['ResultPod',['../class_constructorio___n_e_t_1_1_models_1_1_result_pod.html',1,'Constructorio_NET::Models']]],
-  ['retrievesearchabilitiesrequest_13',['RetrieveSearchabilitiesRequest',['../class_constructorio___n_e_t_1_1_models_1_1_retrieve_searchabilities_request.html',1,'Constructorio_NET::Models']]]
+  ['recommendationstrategy_5',['RecommendationStrategy',['../class_constructorio___n_e_t_1_1_models_1_1_recommendation_strategy.html',1,'Constructorio_NET::Models']]],
+  ['redirect_6',['Redirect',['../class_constructorio___n_e_t_1_1_models_1_1_redirect.html',1,'Constructorio_NET::Models']]],
+  ['redirectdata_7',['RedirectData',['../class_constructorio___n_e_t_1_1_models_1_1_redirect_data.html',1,'Constructorio_NET::Models']]],
+  ['refinedcontent_8',['RefinedContent',['../class_constructorio___n_e_t_1_1_models_1_1_refined_content.html',1,'Constructorio_NET::Models']]],
+  ['result_9',['Result',['../class_constructorio___n_e_t_1_1_models_1_1_result.html',1,'Constructorio_NET::Models']]],
+  ['resultdata_10',['ResultData',['../class_constructorio___n_e_t_1_1_models_1_1_result_data.html',1,'Constructorio_NET::Models']]],
+  ['resultfacet_11',['ResultFacet',['../class_constructorio___n_e_t_1_1_models_1_1_result_facet.html',1,'Constructorio_NET::Models']]],
+  ['resultgroup_12',['ResultGroup',['../class_constructorio___n_e_t_1_1_models_1_1_result_group.html',1,'Constructorio_NET::Models']]],
+  ['resultpod_13',['ResultPod',['../class_constructorio___n_e_t_1_1_models_1_1_result_pod.html',1,'Constructorio_NET::Models']]],
+  ['retrievesearchabilitiesrequest_14',['RetrieveSearchabilitiesRequest',['../class_constructorio___n_e_t_1_1_models_1_1_retrieve_searchabilities_request.html',1,'Constructorio_NET::Models']]]
 ];
