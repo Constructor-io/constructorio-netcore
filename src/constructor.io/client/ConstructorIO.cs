2491e864ad0ce97ff8c6341c300e99839c7f16cf
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2025-09-16 17:29:22 -0400

    Revert "feat: standardize naming conventions (non-breaking)"
    This reverts commit 856d496fdb87b09f8a5ff9499e661a6dcf7cf7ad.

--- src/constructor.io/client/ConstructorIO.cs
+++ src/constructor.io/client/ConstructorIO.cs
@@ -12,7 +12,7 @@ namespace Constructorio_NET
     /// </summary>
     public class ConstructorIO
     {
-        private readonly string Version = "constructorio-net-3.11.1";
+        private readonly string Version = "cionet-3.11.1";
         public Autocomplete Autocomplete { get; }
         public Browse Browse { get; }
         public Catalog Catalog { get; }
