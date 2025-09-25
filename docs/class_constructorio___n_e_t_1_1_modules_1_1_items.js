ff528679e2221e617bf22f9f66ecbd76be4f9987
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2025-09-25 16:22:53 -0500

    Version bump

--- docs/class_constructorio___n_e_t_1_1_modules_1_1_items.js
+++ docs/class_constructorio___n_e_t_1_1_modules_1_1_items.js
@@ -3,6 +3,8 @@ var class_constructorio___n_e_t_1_1_modules_1_1_items =
     [ "Items", "class_constructorio___n_e_t_1_1_modules_1_1_items.html#a65a0863137b20e12ceba6517099faded", null ],
     [ "CreateOrReplaceItems", "class_constructorio___n_e_t_1_1_modules_1_1_items.html#a620c72456c0b37f9b2c6798539a1368f", null ],
     [ "CreateOrReplaceVariations", "class_constructorio___n_e_t_1_1_modules_1_1_items.html#ada40a310a1d39d08e9e04fe48c8deb9e", null ],
+    [ "DeleteItems", "class_constructorio___n_e_t_1_1_modules_1_1_items.html#a3ada11cba0adfc09899e786a899fb87d", null ],
+    [ "DeleteItems", "class_constructorio___n_e_t_1_1_modules_1_1_items.html#afaabd1830f2a61e713bdecb2b644fe8a", null ],
     [ "DeleteItems", "class_constructorio___n_e_t_1_1_modules_1_1_items.html#ac676ca89c70bb3b8420492dff622a44c", null ],
     [ "DeleteVariations", "class_constructorio___n_e_t_1_1_modules_1_1_items.html#aa3cbb9f56dafb8d56f218a25e5b8d309", null ],
     [ "RetrieveItems", "class_constructorio___n_e_t_1_1_modules_1_1_items.html#a5ad86b474e26685e4506a8ecc69df24a", null ],
