26783333afe89392a411cda9436dcbfd18e1fb25
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2025-10-28 10:56:21 +0300

    Version bump

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['makehttprequest_0',['MakeHttpRequest',['../class_constructorio___n_e_t_1_1_utils_1_1_helpers.html#ac9b1040d50fa2f6baacc75aee4e2e461',1,'Constructorio_NET::Utils::Helpers']]],
-  ['makeurl_1',['MakeUrl',['../class_constructorio___n_e_t_1_1_utils_1_1_helpers.html#a00e110490f7d95fd842eaba636eec0ce',1,'Constructorio_NET::Utils::Helpers']]],
-  ['match_2',['Match',['../class_constructorio___n_e_t_1_1_models_1_1_match.html',1,'Constructorio_NET::Models']]],
-  ['matchtype_3',['MatchType',['../class_constructorio___n_e_t_1_1_models_1_1_facet.html#a2e2bc950a1224482dff04927aa70d9fa',1,'Constructorio_NET::Models::Facet']]],
-  ['multiple_4',['Multiple',['../namespace_constructorio___n_e_t_1_1_models.html#ad434f5d919c50eafe52f9e72ce4a23ecaa0bf169f2539e893e00d7b1296bc4d8e',1,'Constructorio_NET::Models']]]
+  ['makehttprequest_3c_20t_20_3e_1',['MakeHttpRequest&lt; T &gt;',['../class_constructorio___n_e_t_1_1_utils_1_1_helpers.html#ad792047949db1e36fc74467470d3ede2',1,'Constructorio_NET::Utils::Helpers']]],
+  ['makeurl_2',['MakeUrl',['../class_constructorio___n_e_t_1_1_utils_1_1_helpers.html#a00e110490f7d95fd842eaba636eec0ce',1,'Constructorio_NET::Utils::Helpers']]],
+  ['match_3',['Match',['../class_constructorio___n_e_t_1_1_models_1_1_match.html',1,'Constructorio_NET::Models']]],
+  ['matchtype_4',['MatchType',['../class_constructorio___n_e_t_1_1_models_1_1_facet.html#a2e2bc950a1224482dff04927aa70d9fa',1,'Constructorio_NET::Models::Facet']]],
+  ['multiple_5',['Multiple',['../namespace_constructorio___n_e_t_1_1_models.html#ad434f5d919c50eafe52f9e72ce4a23ecaa0bf169f2539e893e00d7b1296bc4d8e',1,'Constructorio_NET::Models']]]
 ];
--- docs/search/functions_7.js
+++ docs/search/functions_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['makehttprequest_0',['MakeHttpRequest',['../class_constructorio___n_e_t_1_1_utils_1_1_helpers.html#ac9b1040d50fa2f6baacc75aee4e2e461',1,'Constructorio_NET::Utils::Helpers']]],
-  ['makeurl_1',['MakeUrl',['../class_constructorio___n_e_t_1_1_utils_1_1_helpers.html#a00e110490f7d95fd842eaba636eec0ce',1,'Constructorio_NET::Utils::Helpers']]]
+  ['makehttprequest_3c_20t_20_3e_1',['MakeHttpRequest&lt; T &gt;',['../class_constructorio___n_e_t_1_1_utils_1_1_helpers.html#ad792047949db1e36fc74467470d3ede2',1,'Constructorio_NET::Utils::Helpers']]],
+  ['makeurl_2',['MakeUrl',['../class_constructorio___n_e_t_1_1_utils_1_1_helpers.html#a00e110490f7d95fd842eaba636eec0ce',1,'Constructorio_NET::Utils::Helpers']]]
 ];
